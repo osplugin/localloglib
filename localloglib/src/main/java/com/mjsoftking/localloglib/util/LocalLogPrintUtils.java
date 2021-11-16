9951bd591c33da78fcac8c0dbd794ae242d7ba2b
mjsoftking <1406993111@qq.com>
2021-11-16 16:38:13 +0800

    增加打印公共工具类，针对特定情况下不打印部分日志

--- localloglib/src/main/java/com/mjsoftking/localloglib/util/LocalLogPrintUtils.java
+++ localloglib/src/main/java/com/mjsoftking/localloglib/util/LocalLogPrintUtils.java
@@ -15,75 +15,193 @@ import org.greenrobot.eventbus.EventBus;
 public class LocalLogPrintUtils {
 
     public static void commonPrintf(String msg) {
-        EventBus.getDefault().post(new LogCommonEvent(msg));
+        commonPrintf(msg, null, true);
     }
 
     public static void commonPrintf(String msg, Throwable e) {
-        EventBus.getDefault().post(new LogCommonEvent(msg, e));
+        commonPrintf(msg, e, true);
     }
 
     public static void commonPrintf(Throwable e) {
-        EventBus.getDefault().post(new LogCommonEvent(e));
+        commonPrintf(null, e, true);
     }
 
-    public static void customPrintf(String logFileLevel,String msg) {
-        EventBus.getDefault().post(new LogCustomEvent(logFileLevel, msg));
+    public static void commonPrintf(String msg, boolean print) {
+        commonPrintf(msg, null, print);
     }
 
-    public static void customPrintf(String logFileLevel,String msg, Throwable e) {
-        EventBus.getDefault().post(new LogCustomEvent(logFileLevel, msg, e));
+    /**
+     * @param msg   文本描述
+     * @param e     异常对象
+     * @param print 是否打印，针对特定模式下不需要打印部分日志
+     */
+    public static void commonPrintf(String msg, Throwable e, boolean print) {
+        if (print) {
+            EventBus.getDefault().post(new LogCommonEvent(msg, e));
+        }
+    }
+
+    public static void commonPrintf(Throwable e, boolean print) {
+        commonPrintf(null, e, print);
+    }
+
+
+    public static void customPrintf(String logFileLevel, String msg) {
+        customPrintf(logFileLevel, msg, null, true);
+    }
+
+    public static void customPrintf(String logFileLevel, String msg, Throwable e) {
+        customPrintf(logFileLevel, msg, e, true);
     }
 
     public static void customPrintf(String logFileLevel, Throwable e) {
-        EventBus.getDefault().post(new LogCustomEvent(logFileLevel, e));
+        customPrintf(logFileLevel, null, e, true);
+    }
+
+    public static void customPrintf(String logFileLevel, String msg, boolean print) {
+        customPrintf(logFileLevel, msg, null, print);
+    }
+
+    /**
+     * @param msg   文本描述
+     * @param e     异常对象
+     * @param print 是否打印，针对特定模式下不需要打印部分日志
+     */
+    public static void customPrintf(String logFileLevel, String msg, Throwable e, boolean print) {
+        if (print) {
+            EventBus.getDefault().post(new LogCustomEvent(logFileLevel, msg, e));
+        }
+    }
+
+    public static void customPrintf(String logFileLevel, Throwable e, boolean print) {
+        customPrintf(logFileLevel, null, e, print);
     }
 
 
     public static void debugPrintf(String msg) {
-        EventBus.getDefault().post(new LogDebugEvent(msg));
+        debugPrintf(msg, null, true);
     }
 
     public static void debugPrintf(String msg, Throwable e) {
-        EventBus.getDefault().post(new LogDebugEvent(msg, e));
+        debugPrintf(msg, e, true);
     }
 
     public static void debugPrintf(Throwable e) {
-        EventBus.getDefault().post(new LogDebugEvent(e));
+        debugPrintf(null, e, true);
     }
 
+    public static void debugPrintf(String msg, boolean print) {
+        debugPrintf(msg, null, print);
+    }
+
+    /**
+     * @param msg   文本描述
+     * @param e     异常对象
+     * @param print 是否打印，针对特定模式下不需要打印部分日志
+     */
+    public static void debugPrintf(String msg, Throwable e, boolean print) {
+        if (print) {
+            EventBus.getDefault().post(new LogDebugEvent(msg, e));
+        }
+    }
+
+    public static void debugPrintf(Throwable e, boolean print) {
+        debugPrintf(null, e, print);
+    }
+
+
     public static void infoPrintf(String msg) {
-        EventBus.getDefault().post(new LogInfoEvent(msg));
+        infoPrintf(msg, null, true);
     }
 
     public static void infoPrintf(String msg, Throwable e) {
-        EventBus.getDefault().post(new LogInfoEvent(msg, e));
+        infoPrintf(msg, e, true);
     }
 
     public static void infoPrintf(Throwable e) {
-        EventBus.getDefault().post(new LogInfoEvent(e));
+        infoPrintf(null, e, true);
     }
 
+    public static void infoPrintf(String msg, boolean print) {
+        infoPrintf(msg, null, print);
+    }
+
+    /**
+     * @param msg   文本描述
+     * @param e     异常对象
+     * @param print 是否打印，针对特定模式下不需要打印部分日志
+     */
+    public static void infoPrintf(String msg, Throwable e, boolean print) {
+        if (print) {
+            EventBus.getDefault().post(new LogInfoEvent(msg, e));
+        }
+    }
+
+    public static void infoPrintf(Throwable e, boolean print) {
+        infoPrintf(null, e, print);
+    }
+
+
     public static void runtimePrintf(String msg) {
-        EventBus.getDefault().post(new LogRuntimeEvent(msg));
+        runtimePrintf(msg, null, true);
     }
 
     public static void runtimePrintf(String msg, Throwable e) {
-        EventBus.getDefault().post(new LogRuntimeEvent(msg, e));
+        runtimePrintf(msg, e, true);
     }
 
     public static void runtimePrintf(Throwable e) {
-        EventBus.getDefault().post(new LogRuntimeEvent(e));
+        runtimePrintf(null, e, true);
     }
 
+    public static void runtimePrintf(String msg, boolean print) {
+        runtimePrintf(msg, null, print);
+    }
+
+    /**
+     * @param msg   文本描述
+     * @param e     异常对象
+     * @param print 是否打印，针对特定模式下不需要打印部分日志
+     */
+    public static void runtimePrintf(String msg, Throwable e, boolean print) {
+        if (print) {
+            EventBus.getDefault().post(new LogRuntimeEvent(msg, e));
+        }
+    }
+
+    public static void runtimePrintf(Throwable e, boolean print) {
+        runtimePrintf(null, e, print);
+    }
+
+
     public static void exceptionPrintf(String msg) {
-        EventBus.getDefault().post(new LogExceptionEvent(msg));
+        exceptionPrintf(msg, null, true);
     }
 
     public static void exceptionPrintf(String msg, Throwable e) {
-        EventBus.getDefault().post(new LogExceptionEvent(msg, e));
+        exceptionPrintf(msg, e, true);
     }
 
     public static void exceptionPrintf(Throwable e) {
-        EventBus.getDefault().post(new LogExceptionEvent(e));
+        exceptionPrintf(null, e, true);
+    }
+
+    public static void exceptionPrintf(String msg, boolean print) {
+        exceptionPrintf(msg, null, print);
+    }
+
+    /**
+     * @param msg   文本描述
+     * @param e     异常对象
+     * @param print 是否打印，针对特定模式下不需要打印部分日志
+     */
+    public static void exceptionPrintf(String msg, Throwable e, boolean print) {
+        if (print) {
+            EventBus.getDefault().post(new LogExceptionEvent(msg, e));
+        }
+    }
+
+    public static void exceptionPrintf(Throwable e, boolean print) {
+        exceptionPrintf(null, e, print);
     }
 }
